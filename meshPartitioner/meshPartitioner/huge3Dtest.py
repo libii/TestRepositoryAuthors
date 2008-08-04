e974c98c7c72e1ba8e98b4c913b864bd3dcf1463
Kevin Milner <kmilner@usc.edu>
2008-08-04 16:49:10 +0000

    added partitioning directy to file

--- meshPartitioner/meshPartitioner/huge3Dtest.py
+++ meshPartitioner/meshPartitioner/huge3Dtest.py
@@ -4,7 +4,7 @@ import os
 from meshPartitioner import MeshPartitioner
 from mesh3D.meshTools3D import MeshTools3D
 from meshToolsAPI import TYPE_FLOAT
-import utils.fileUtils as fileUtils
+import util.fileUtils as fileUtils
 
 partition = False
 
@@ -18,11 +18,13 @@ if not os.path.exists(meshFile):
 	print "Writing " + meshFile
 	tools.writeTestMesh(meshFile, dims)
 
-pieces = [100, 100, 100]
+pieces = [2, 2, 1]
 
 meshDir = meshFile + "_PARTS"
 
 if os.path.exists(meshDir):
-	fileUtils.rmdir(meshDir)
+	fileUtils.rmdir(meshDir, verbose=True)
 
 parter = MeshPartitioner(tools, meshFile, meshDir, dims, pieces)
+
+parter.partition()
