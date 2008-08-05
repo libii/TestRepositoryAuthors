faac2f1bec57d2adfc8e11b1ea274ba3cb473399
Kevin Milner <kmilner@usc.edu>
2008-08-05 22:13:29 +0000

    pbs script creation

---
+++ pbsScriptCreator.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import os, sys, math
+
+import meshPartitioner.conf.confLoader as confLoader
+
+from optparse import OptionParser
+
+path = os.path.abspath(os.path.dirname(sys.argv[0]))
+if not path.endswith(os.sep):
+	path = path + os.sep
+
+parser = OptionParser()
+
+parser.add_option("-c", "--conf", dest="conf_file", default=confLoader.DEFAULT_CONF_FILE_NAME, type="string",
+			  help="Configuration File (default = %default)")
+parser.add_option("-d", "--output-dir", dest="output_dir", default=".", type="string",
+			  help="Output Dir (default = %default)")
+
+progName = sys.argv[0]
+usage = progName + ""
+usage = progName + " NUM_JOBS WALL_TIME_MINUTES"
+
+parser.set_usage(usage)
+
+(options, args) = parser.parse_args()
+
+outputDir = options.output_dir
+if not outputDir.endswith(os.sep):
+	outputDir += os.sep
+
+if not os.path.exists(outputDir):
+	os.mkdir(outputDir)
+
+conf = confLoader.ConfLoader(options.conf_file)
+
+if len(args) != 2:
+	parser.print_usage()
+	sys.exit(1)
+
+numJobs = int(args[0])
+
+if numJobs <= 0:
+	print "The number of jobs must be a positive integer!"
+	parser.print_usage()
+	sys.exit(1)
+
+wallTimeMinsTotal = int(args[1])
+
+if numJobs < 1:
+	print "The wall time must be a integer >= 1"
+	parser.print_usage()
+	sys.exit(1)
+
+wallTimeHours = int(wallTimeMinsTotal / 60)
+wallTimeMins = wallTimeMinsTotal % 60
+
+def padNum(num, digits):
+	numStr = str(num)
+	while len(numStr) < digits:
+		numStr = "0" + numStr
+	return numStr
+
+minsStr = padNum(wallTimeMins, 2)
+
+hoursStr = padNum(wallTimeHours, 2)
+
+wallTime = hoursStr + ":" + minsStr + ":00"
+
+digits = len(str(numJobs))
+
+dims = conf.getInputMeshDimensions()
+pieces = conf.getPartitions()
+
+numPieces = 1
+
+for piece in pieces:
+	numPieces *= piece
+
+print "Making jobs for " + str(numPieces) + " pieces"
+
+if numJobs > numPieces:
+	print "ERROR: There are more jobs than pieces! (" + str(numJobs) + " jobs, " + str(numPieces) + " pieces)"
+	sys.exit(1)
+
+def writeFile(count, digits, confFile, outputDir, startIndex, endIndex):
+	name = outputDir + "partition_" + padNum(count, digits)
+	fp = open(name + ".qsub", "w")
+	fp.write("#!/bin/bash" + "\n")
+	fp.write("#" + "\n")
+	fp.write("#PBS -l walltime=" + wallTime + "\n")
+	fp.write("#PBS -o " + name + ".out" + "\n")
+	fp.write("#PBS -e " + name + ".err" + "\n")
+	fp.write("\n")
+	partCommand = path + "partition.py -c " + confFile + " -a " + str(startIndex) + " " + str(endIndex)
+	print partCommand
+	fp.write(partCommand + "\n")
+	fp.close()
+
+piecesPerJob = int(math.ceil(float(numPieces) / float(numJobs)))
+print str(piecesPerJob) + " pieces per job"
+for i in range(0, numJobs):
+	startIndex = i * piecesPerJob
+	endIndex = startIndex + piecesPerJob - 1
+	
+	writeFile(i, digits, conf.getConfFileName(), outputDir, startIndex, endIndex)
